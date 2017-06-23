d9a9961062ed3d73ff09930a315d8c1bcdc5cd91
Sam Mitchell Finnigan <s.j.finnigan@ncl.ac.uk>
2017-06-23 16:34:33 +0100

    Add unit tests for skew and scale_xy to test_transform

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -24,3 +24,21 @@ def test_group_class():
     group = svg_fig.getroot()
     ok_((group.root.attrib['class'] == 'main'))
 
+def test_skew():
+    svg_fig = transform.fromstring(circle)
+    group = svg_fig.getroot()
+
+    # Test skew in y-axis
+    group.skew(0, 30)
+    ok_('skewY(30' in group.root.get('transform'))
+
+    # Test skew in x-axis
+    group.skew(30, 0)
+    ok_('skewX(30' in group.root.get('transform'))
+
+def test_scale_xy():
+    svg_fig = transform.fromstring(circle)
+    group = svg_fig.getroot()
+
+    group.scale_xy(0, 30)
+    ok_('scale(0' in group.root.get('transform'))
