94fbba69d4283122a33dde96ee6c8af4ab8a7d7b
Thawann Malfatti <malfatti@disroot.org>
2020-06-18 16:06:54 -0300

    Enable huge trees

--- src/svgutils/transform.py
+++ src/svgutils/transform.py
@@ -314,7 +314,7 @@ def fromfile(fname):
     """
     fig = SVGFigure()
     with open(fname) as fid:
-        svg_file = etree.parse(fid)
+        svg_file = etree.parse(fid, parser=etree.XMLParser(huge_tree=True))
 
     fig.root = svg_file.getroot()
     return fig
@@ -335,7 +335,7 @@ def fromstring(text):
         content.
     """
     fig = SVGFigure()
-    svg = etree.fromstring(text.encode())
+    svg = etree.fromstring(text.encode(), parser=etree.XMLParser(huge_tree=True))
 
     fig.root = svg
 
