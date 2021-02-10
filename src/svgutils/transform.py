btel/svg_utils

commit 6f5d10fa5cb21f7fb72ee256d0371ba9d5943627

fix for #57

src/svgutils/transform.py
@@ -239,11 +239,17 @@ class SVGFigure(object):
         self._height = 0
 
         if width:
-            self._width = width.value
-            self.width = width
+            try:
+                # width is an instance of Unit
+                self._width = width.value
+            except AttributeError:
+                # int or str
+                self._width = width
         if height:
-            self._height = height.value
-            self.height = height
+            try:
+                self._height = height.value
+            except AttributeError:
+                self._height = height
 
     @property
     def width(self):

tests/test_transform.py
@@ -57,3 +57,6 @@ def test_create_svg_figure():
 
     svg_fig = transform.SVGFigure(2, 3)
     assert "svg" in svg_fig.to_str().decode("ascii")
+
+    svg_fig = transform.SVGFigure("2", "3")
+    assert "svg" in svg_fig.to_str().decode("ascii")

commit 8c1696a3a995c2517b632df961fe08a14e0bb7ca

transform.py: Get width/height/viewBox included with SVG output again (fixes #61)
Regression from commit 6f5d10fa5cb21f7fb72ee256d0371ba9d5943627
introduce as part of pull request #58.

src/svgutils/transform.py
@@ -240,14 +240,14 @@ class SVGFigure(object):
 
         if width:
             try:
-                # width is an instance of Unit
-                self._width = width.value
+                self.width = width  # this goes to @width.setter a few lines down
             except AttributeError:
                 # int or str
                 self._width = width
+
         if height:
             try:
-                self._height = height.value
+                self.height = height  # this goes to @height.setter a few lines down
             except AttributeError:
                 self._height = height
 